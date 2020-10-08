a9e1a4fa289714d0dbc5aeab00e5611d0bf39f02
Yakini <yakinilatha09@gmail.com>
2020-10-08 15:02:50 -0600

    changed the router name

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";

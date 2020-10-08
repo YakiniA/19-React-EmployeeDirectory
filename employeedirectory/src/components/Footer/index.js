4704c8558ffe8237ff20fa9656d6e43ef4978f18
Yakini <yakinilatha09@gmail.com>
2020-10-08 17:00:03 -0600

    made more responsive with styling

--- employeedirectory/src/components/Footer/index.js
+++ employeedirectory/src/components/Footer/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
+    <footer className="footer fixed-bottom">
      Copyright Employee Directory &copy; 2020 
     </footer>
   );
